e127e351e2a2e1faecb7a67f8a42eabab8f6af88
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-12-08 22:43:43 +0545

    config

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors"; 
+import dotenv from 'dotenv'
 import dataRoute from "./routes/getData.js";
 
+dotenv.config()
 const app = express();
+app.use(cors()); 
 app.use(express.json());
 app.use(express.static("assets/img"));
 
 app.use("/api", dataRoute);
 
-app.listen(8080, () => {
-  console.log("running @ http://localhost:8080/api");
+app.listen(process.env.PORT, () => {
+  console.log(`running @ http://localhost:${process.env.PORT}/api`);
 });
