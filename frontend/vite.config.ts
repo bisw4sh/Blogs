fd589a36e9166e42a919d334f0263d2e36d34d73
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-12-09 12:04:04 +0545

    url fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,8 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target:
-          "http://localhost:8080/api",
+        target: "https://blogs-backedn.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
