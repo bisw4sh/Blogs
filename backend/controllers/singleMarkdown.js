d923139a8be4c58f0ecfee671ba645d23283e03c
Biswash <spikeystona@gmail.com>
2023-12-12 12:36:15 +0545

    can't access params in router & controllers but can in index

---
+++ backend/controllers/singleMarkdown.js
@@ -0,0 +1,11 @@
+// import fs from "fs/promises";
+// import path from "path";
+// import matter from "gray-matter";
+// import showdown from "showdown";
+
+export default async function singleMarkdown(id) {
+  console.log("single Markdown js")
+  const ID = id;
+
+  return parseInt(ID);
+}
