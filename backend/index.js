5fbbd8700d17a5af28afb6da9671af2e2f905cbc
Biswash <spikeystona@gmail.com>
2023-12-12 12:34:53 +0545

    added api endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,18 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import dataRoute from "./routes/getData.js";
+import getMarkdown from "./routes/getMarkdown.js";
 
-dotenv.config();
 const app = express();
-app.use(
-  cors(
-    {origin: [
-      "https://api.render.com",
-      "http://api.render.com",
-      "http://localhost:8080",
-      "https://blogs-frontedn.onrender.com/",
-      "https://blogs-frontedn.onrender.com/api",
-    ],
-    credentials: true,}
-  )
-);
-
 app.use(express.json());
-app.use(express.static("assets/img"));
 
 app.use("/api", dataRoute);
+app.use("/api/:id", getMarkdown);
+// app.use("/api/:id", (req, res) => {
+//   res.json( {
+//     msg : req.params.id
+//   })
+// });
 
 app.listen(process.env.PORT, () => {
-  console.log(`running @ http://localhost:${process.env.PORT}/api`);
+  console.log(`running @ http://localhost:${process.env.PORT}`);
 });
